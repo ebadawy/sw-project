10fdc8febdd3dd90346dfe1b595e2556cd83692a
=khaledba5 <eng.khaled.93@hotmail.com>
2015-02-03 14:32:39 +0200

    s

--- app/src/main/java/com/example/khaled/takequiz/studentresult.java
+++ app/src/main/java/com/example/khaled/takequiz/studentresult.java
@@ -2,15 +2,21 @@ package com.example.khaled.takequiz;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android
 
 
 public class studentresult extends ActionBarActivity {
 
+    public void msg(View view){
+        DialogFragment builder = new popup();
+        builder.show(getSupportFragmentManager(),"Result");
+    }
 
 
     @Override
