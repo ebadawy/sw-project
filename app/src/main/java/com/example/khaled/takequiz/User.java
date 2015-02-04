5ea8a6fccc8156d0c5fd933e9de7637d63c47716
ehab93 <ehalbadawy93@gmail.com>
2015-02-04 11:26:08 +0200

    Issue #10, Edit User class

--- app/src/main/java/com/example/khaled/takequiz/User.java
+++ app/src/main/java/com/example/khaled/takequiz/User.java
@@ -2,23 +2,23 @@ package com.example.khaled.takequiz;
 
 
 public class User {
-    private String user_id;
+    private String user_name;
     private String password;
 
     public User() {
-        user_id = null;
-        password = null;
+        this.user_name = null;
+        this.password = null;
     }
 
-    public User(String id, String pass) {
-        user_id = id;
-        password = pass;
+    public User(String name, String pass) {
+        this.user_name = name;
+        this.password = pass;
     }
 
-    public String getUserID() {return user_id;}
+    public String getUserName() {return user_name;}
     public String getPassword() {return password;}
 
-    public void setUserId(String id) {user_id = id;}
-    public void setPassword(String pass) {password = pass;}
+    public void setUserName(String id) {this.user_name = id;}
+    public void setPassword(String pass) {this.password = pass;}
 
 }
