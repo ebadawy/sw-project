c07d3bd128b957a3168a03ecf071a7aaaf4b67b5
hatem93 <hatem.hussin1993@gmail.com>
2015-02-04 09:59:16 +0200

    Issue #7,quizDoctorQuestionAnswer

---
+++ app/src/main/java/com/example/khaled/takequiz/Fragment1.java
@@ -0,0 +1,108 @@
+package com.example.khaled.takequiz;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by user on 2/2/2015.
+ */
+public class Fragment1 extends Fragment implements View.OnClickListener{
+    private LinearLayout mLayout;
+    private Button mButton;
+    private RadioGroup radioGroup;
+    long counter=1;
+    List<EditText> allEds = new ArrayList<EditText>();
+    List<RadioButton> allRbs = new ArrayList<RadioButton>();
+    Button check;
+
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+     Bundle savedInstanceState)
+    { View beko = (ScrollView)inflater.inflate(R.layout.fragment1_layout,
+            container,false);
+        mLayout = (LinearLayout) beko.findViewById(R.id.linear1);
+        mButton = (Button)beko.findViewById(R.id.addChoice);
+        radioGroup = (RadioGroup)beko.findViewById(R.id.radioGroup);
+        check = (Button)beko.findViewById(R.id.check);
+        mButton.setOnClickListener(this);
+        check.setOnClickListener(this);
+
+         return beko;
+    }
+
+
+    public void addChoiceclick(){
+
+        EditText editText = new EditText(getActivity() );
+        RadioButton radiobutton = new RadioButton(getActivity());
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        radiobutton.setLayoutParams(p);
+        editText.setLayoutParams(p);
+        editText.setText("Write Choice Here");
+        editText.setId((int) counter);
+        counter++;
+        radiobutton.setId((int) counter);
+        counter++;
+        allEds.add(editText);
+        allRbs.add(radiobutton);
+        Log.d("View", "Start");
+
+        try{
+            radioGroup.addView(radiobutton);
+            radioGroup.addView(editText);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void checkClick(){
+        if (radioGroup.getCheckedRadioButtonId() == -1)
+        {
+            Toast.makeText(getActivity(), "Choose Right Answer", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        else
+        {
+            Toast.makeText(getActivity(), "Check Passed", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+     public void onClick(View view) {
+                switch(view.getId()){
+                    case R.id.addChoice:
+                        addChoiceclick();
+                        break;
+                    case R.id.check:
+                        checkClick();
+                        break;
+                }
+
+            }
+
+
+
+}
+
+
+
+
+
--- app/src/main/java/com/example/khaled/takequiz/QuizQuestionAnswerDoctor.java
+++ app/src/main/java/com/example/khaled/takequiz/QuizQuestionAnswerDoctor.java
@@ -1,39 +1,133 @@
 package com.example.khaled.takequiz;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Vector;
 
 
-public class QuizQuestionAnswerDoctor extends ActionBarActivity {
-
+public class QuizQuestionAnswerDoctor extends FragmentActivity implements View.OnClickListener , OnPageChangeListener{
+    Button addQuestion;
+    private PagerAdapter mPagerAdapter;
+    private List<Fragment> fragments;
+    Button nextPage;
+    Button previousPage;
+    ViewPager pager;
+    int currentPage;
+    EditText pageNumber;
+    Button go;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_question_answer_doctor);
+        addQuestion =(Button)findViewById(R.id.addQuestion);
+        nextPage=(Button)findViewById(R.id.nextPage);
+        previousPage=(Button)findViewById(R.id.previousPage);
+        go=(Button)findViewById(R.id.go);
+        pageNumber = (EditText)findViewById(R.id.pageNumber);
+        addQuestion.setOnClickListener(this);
+        initialisePaging();
+        nextPage.setOnClickListener(this);
+        previousPage.setOnClickListener(this);
+        go.setOnClickListener(this);
+
     }
 
+    private void initialisePaging() {
+        fragments = new Vector<Fragment>();
+        fragments.add(Fragment.instantiate(this,Fragment1.class.getName()));
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz_question_answer_doctor, menu);
-        return true;
+        mPagerAdapter = new PagerAdapter(this.getSupportFragmentManager(),fragments);
+        ViewPager pager = (ViewPager)findViewById(R.id.viewpager);
+        pager.setAdapter(mPagerAdapter);
+        pager.setOnPageChangeListener(this);
+
+
     }
+    private void addQuestionClick(){
+        fragments.add(Fragment.instantiate(this,Fragment1.class.getName()));
+        mPagerAdapter.notifyDataSetChanged();
+        Toast.makeText(getApplicationContext(), "Question Added Swipe", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    }
+    private void goClick(){
+        String pageNumber_ = pageNumber.getText().toString();
+        int pageNumberx = Integer.parseInt(pageNumber_);
+        ViewPager pager = (ViewPager)findViewById(R.id.viewpager);
+        pager.setCurrentItem(pageNumberx-1,true);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    }
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.addQuestion:
+                addQuestionClick();
+                break;
+
+            case R.id.nextPage:
+                nextPage();
+                break;
+            case R.id.previousPage:
+                previousPage();
+                break;
+            case R.id.go:
+                goClick();
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Toast.makeText(getApplicationContext(), "Question " + (position+1), Toast.LENGTH_SHORT).show();
+        currentPage = position;
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+    private void nextPage() {
+
+        int totalPages = mPagerAdapter.getCount();
+
+        int nextPage = currentPage+1;
+        if (nextPage >= totalPages) {
+            // We can't go forward anymore.
+            // Loop to the first page. If you don't want looping just
+            // return here.
+            nextPage = 0;
+        }
+        ViewPager pager = (ViewPager)findViewById(R.id.viewpager);
+        pager.setCurrentItem(nextPage, true);
+    }
+
+    private void previousPage() {
+
+
+        int totalPages = mPagerAdapter.getCount();
+
+        int previousPage = currentPage-1;
+        if (previousPage < 0) {
+            // We can't go back anymore.
+            // Loop to the last page. If you don't want looping just
+            // return here.
+            previousPage = totalPages - 1;
+        }
+        ViewPager pager = (ViewPager)findViewById(R.id.viewpager);
+        pager.setCurrentItem(previousPage,true);
     }
 }
