d6c375dfdd9de640ab1ff4407842ca2ebfbdff0b
ehab93 <ehalbadawy93@gmail.com>
2015-02-04 11:28:04 +0200

    Issue #10, Create POST and GET requests

--- app/src/main/java/com/example/khaled/takequiz/QuizAPI.java
+++ app/src/main/java/com/example/khaled/takequiz/QuizAPI.java
@@ -3,9 +3,21 @@ package com.example.khaled.takequiz;
 import java.util.List;
 
 import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.http.Body;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Headers;
+import retrofit.http.POST;
 
 public interface QuizAPI {
+    @POST("/users")
+    @Headers({"Content-Type: application/json"})
+    public void createUser(@Body RequestWraper user,
+                           Callback<RequestWraper> response);
+
     @GET("/users")
-    public void getUser(Callback<List<User>> respone);
+    public void getUsers(Callback<List<User>> users);
 }
