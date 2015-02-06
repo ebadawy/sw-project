b8ff21bc248fb989ea36e32b5c65c20f96cb3377
ehab93 <ehalbadawy93@gmail.com>
2015-02-06 17:42:42 +0200

    Issue #10, Move User class to model folder and Create its wraper

---
+++ app/src/main/java/com/rest/model/UserWraper.java
@@ -0,0 +1,12 @@
+package com.rest.model;
+
+/**
+ * Created by ehab on 2/4/15.
+ */
+public class UserWraper {
+    private User user;
+
+    public UserWraper(User u) {
+        this.user = u;
+    }
+}
