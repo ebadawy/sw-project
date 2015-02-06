5e3c9f169ea15b049b03addd3d136360d3667e2c
ehab93 <ehalbadawy93@gmail.com>
2015-02-06 17:33:42 +0200

    Issue #10, Add login connection

--- app/src/main/java/com/example/khaled/takequiz/QuizAPI.java
+++ app/src/main/java/com/example/khaled/takequiz/QuizAPI.java
@@ -1,23 +1,31 @@
 package com.example.khaled.takequiz;
 
+import com.google.gson.JsonElement;
+import com.rest.model.StringResponse;
+import com.rest.model.User;
+import com.rest.model.UserWraper;
+import com.squareup.okhttp.Response;
+
 import java.util.List;
 
 import retrofit.Callback;
-import retrofit.RestAdapter;
 import retrofit.http.Body;
-import retrofit.http.Field;
-import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
-import retrofit.http.Header;
 import retrofit.http.Headers;
 import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
 
 public interface QuizAPI {
     @POST("/users")
     @Headers({"Content-Type: application/json"})
-    public void createUser(@Body RequestWraper user,
+    public void createUser(@Body UserWraper user,
                            Callback<RequestWraper> response);
+
 
     @GET("/users")
     public void getUsers(Callback<List<User>> users);
+
+    @GET("/login")
+    public void login(@Query("user_name") String userName, @Query("password") String pass, Callback<User> user);
 }
