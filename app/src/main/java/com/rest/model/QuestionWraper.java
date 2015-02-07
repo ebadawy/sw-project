ce17a8d06026c4b4bf8f4a1211f3b35f49266c82
ehab93 <ehalbadawy93@gmail.com>
2015-02-07 15:35:57 +0200

    Issue #10, Create a wraper for question class

---
+++ app/src/main/java/com/rest/model/QuestionWraper.java
@@ -0,0 +1,12 @@
+package com.rest.model;
+
+/**
+ * Created by ehab on 2/7/15.
+ */
+public class QuestionWraper {
+    private Question question;
+
+    public QuestionWraper(Question q) {
+        this.question = q;
+    }
+}
