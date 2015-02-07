ffd0783479204dd6c3c9513dcac2633afe6bdcd8
ehab93 <ehalbadawy93@gmail.com>
2015-02-07 15:36:35 +0200

    Issue #10, Add the createQuestion method

--- app/src/main/java/com/example/khaled/takequiz/QuizAPI.java
+++ app/src/main/java/com/example/khaled/takequiz/QuizAPI.java
@@ -1,7 +1,7 @@
 package com.example.khaled.takequiz;
 
-import com.google.gson.JsonElement;
-import com.rest.model.StringResponse;
+import com.rest.model.Question;
+import com.rest.model.QuestionWraper;
 import com.rest.model.User;
 import com.rest.model.UserWraper;
 import com.squareup.okhttp.Response;
@@ -13,19 +13,25 @@ import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.Headers;
 import retrofit.http.POST;
-import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface QuizAPI {
+
     @POST("/users")
     @Headers({"Content-Type: application/json"})
     public void createUser(@Body UserWraper user,
-                           Callback<RequestWraper> response);
+                           Callback<Response> response);
 
 
     @GET("/users")
     public void getUsers(Callback<List<User>> users);
 
     @GET("/login")
-    public void login(@Query("user_name") String userName, @Query("password") String pass, Callback<User> user);
+    public void login(@Query("user_name") String userName,
+                      @Query("password") String pass,
+                      Callback<User> user);
+
+    @POST("/questions")
+    public void createQuestion(@Body QuestionWraper questionWraper,
+                                Callback<Response> response);
 }
