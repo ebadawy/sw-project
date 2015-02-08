db1735f7b136bd4bf8f27faeccf457422d8a4c49
ehab93 <ehalbadawy93@gmail.com>
2015-02-08 11:01:37 +0200

    Issue #10, Create loadQuizInfo function

--- app/src/main/java/com/rest/model/Quiz.java
+++ app/src/main/java/com/rest/model/Quiz.java
@@ -1,7 +1,15 @@
 package com.rest.model;
 
 
+import android.widget.Toast;
+
+import com.example.khaled.takequiz.MainActivity;
+
 import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class Quiz {
     private int id;
@@ -9,8 +17,8 @@ public class Quiz {
     private String deadline;
     private String time_limit;
     private int quiz_mark;
-
     private List<Question> questions;
+    
 
     public Quiz() {
         this.name = null;
@@ -44,6 +52,34 @@ public class Quiz {
 
     public int getId() {
         return this.id;
+    }
+
+    public void loadQuizInfo() {
+
+        MainActivity.api.getQuizQuestions(getId(), new Callback<List<Question>>() {
+            @Override
+            public void success(List<Question> questions, Response response) {
+                for(final Question question : questions) {
+                    MainActivity.api.getQuestionChoices(1, question.getId(), new Callback<List<Choice>>() {
+                        @Override
+                        public void success(List<Choice> choices, Response response) {
+                            question.setChoices(choices);
+                        }
+
+                        @Override
+                        public void failure(RetrofitError retrofitError) {
+
+                        }
+                    });
+                }
+                setQuestions(questions);
+            }
+
+            @Override
+            public void failure(RetrofitError retrofitError) {
+
+            }
+        });
     }
 }
 
