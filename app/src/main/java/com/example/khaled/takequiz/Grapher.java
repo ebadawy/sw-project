a2a3c6d4c0dc7c6f384a74b2c2bd467313bdaec0
hatem93 <hatem.hussin1993@gmail.com>
2015-02-11 18:43:43 +0200

    Issue #15, creating a layout for grapher and statistics

---
+++ app/src/main/java/com/example/khaled/takequiz/Grapher.java
@@ -0,0 +1,48 @@
+package com.example.khaled.takequiz;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
+
+
+public class Grapher extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grapher);
+
+        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
+                new GraphViewData(1, 40)
+                , new GraphViewData(2, 12)
+                , new GraphViewData(3, 7)
+                , new GraphViewData(2, 8)
+                , new GraphViewData(2, 10)
+                , new GraphViewData(3, 26)
+                , new GraphViewData(1, 37)
+                , new GraphViewData(1, 53)
+                , new GraphViewData(3, 253)
+
+        });
+
+        GraphView graphView = new LineGraphView(
+                this // context
+                , "Job Status Graph" // heading
+        );
+        graphView.addSeries(exampleSeries); // data
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+        layout.addView(graphView);
+    }
+    }
+
