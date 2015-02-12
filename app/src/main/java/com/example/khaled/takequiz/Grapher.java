c964059862f0950967b7ea91c0d208317143bb91
hatem93 <hatem.hussin1993@gmail.com>
2015-02-12 22:02:45 +0200

    Issue #15, problems in statistics

--- app/src/main/java/com/example/khaled/takequiz/Grapher.java
+++ app/src/main/java/com/example/khaled/takequiz/Grapher.java
@@ -2,11 +2,13 @@ package com.example.khaled.takequiz;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.jjoe64.graphview.GraphView;
 
@@ -36,48 +38,54 @@ public class Grapher extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grapher);
+        GraphView graph = (GraphView)findViewById(R.id.graph);
+        LinearLayout l = (LinearLayout)findViewById(R.id.layout1);
             Bundle extras = getIntent().getExtras();
                 userid = extras.getString("id");
                 username = extras.getString("username");
-
-
         int id = Integer.parseInt(userid);
-
-
-
-
         MainActivity.api.getQuizzes(id, new Callback<List<Quiz>>() {
             @Override
             public void success(List<Quiz> quizs, Response response) {
                 for (int i = 0; i < quizs.size(); i++) {
-                    quizzes.add(quizs.get(i));
+                    quizzes.add(i,quizs.get(i));
                 }
             }
-
             @Override
             public void failure(RetrofitError retrofitError) {
-
             }
         });
-        for (int i = 0; i < quizzes.size(); i++) {
-            MainActivity.api.getResult(id, quizzes.get(i).getId(), new Callback<Result>() {
-                @Override
-                public void success(Result result, Response response) {
-                    quizresult.add(result);
+        System.out.println("quizzes size: " +quizzes.size());
+        //for (int i = 0; i < quizzes.size(); i++) {
+            //MainActivity.api.getResult(id, quizzes.get(i).getId(), new Callback<Result>() {
+               // @Override
+               // public void success(Result result, Response response) {
+                  //  quizresult.add(result);
 
-                }
+               // }
 
-                @Override
-                public void failure(RetrofitError retrofitError) {
+               // @Override
+              //  public void failure(RetrofitError retrofitError) {
 
-                }
-            });
-        }
+             //   }
+           // });
+        //}
 
-        GraphView graph = (GraphView)findViewById(R.id.graph);
-        graph.addSeries(new LineGraphSeries(generateData()));
-        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-        
+
+        //graph.addSeries(new LineGraphSeries(generateData()));
+        //StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        //String[] quizName = new String[quizzes.size()];
+        //for(int i =0 ; i< quizzes.size();i++){
+         //   String name = quizzes.get(i).getName();
+
+          //  quizName[i] = name;
+        //}
+        //staticLabelsFormatter.setHorizontalLabels(quizName);
+        //graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+        //l.addView(graph);
+       // for(int i = 0 ; i<quizzes.size();i++){
+       //     System.out.println(quizzes.get(i).getId()+"        "+quizresult.get(i).getResult());
+       // }
     }
     private DataPoint[] generateData() {
 
