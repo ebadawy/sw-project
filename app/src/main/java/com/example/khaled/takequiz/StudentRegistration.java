aaa0463901b47edf998be8fdd1a7d7997783a02e
=khaledba5 <eng.khaled.93@hotmail.com>
2015-02-13 16:04:43 +0200

    Issue #8,add on item selected function to spinner

--- app/src/main/java/com/example/khaled/takequiz/StudentRegistration.java
+++ app/src/main/java/com/example/khaled/takequiz/StudentRegistration.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 public class StudentRegistration extends ActionBarActivity {
@@ -13,6 +15,18 @@ public class StudentRegistration extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_registration);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
