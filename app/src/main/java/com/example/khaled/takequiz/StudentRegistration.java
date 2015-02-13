47dfff17278bc636f484e4f7b1bef4f7955065f3
=khaledba5 <eng.khaled.93@hotmail.com>
2015-02-13 18:33:38 +0200

    Issue #8,add spinner of groups

--- app/src/main/java/com/example/khaled/takequiz/StudentRegistration.java
+++ app/src/main/java/com/example/khaled/takequiz/StudentRegistration.java
@@ -6,7 +6,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.rest.model.Group;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class StudentRegistration extends ActionBarActivity {
@@ -15,18 +25,36 @@ public class StudentRegistration extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_registration);
-        Spinner spinner = (Spinner)findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        MainActivity.api.getGroups(MainActivity.current_user.getId(),new Callback<List<Group>>() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                
+            public void success(List<Group> groups, Response response) {
+                Spinner spinner = (Spinner)findViewById(R.id.spinner);
+                List<String> names = new ArrayList<String>();
+                for(Group group : groups){
+                    names.add(group.getGroupName());
+                }
+             ArrayAdapter<String> adp =
+                     new ArrayAdapter<String>(StudentRegistration.this,android.R.layout.simple_list_item_1,names);
+                adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void failure(RetrofitError retrofitError) {
 
             }
         });
+
     }
 
 
