27d3d3eaa7486987027f6c15f6f13b70f0e0cd10
ehab93 <ehalbadawy93@gmail.com>
2015-02-14 23:39:20 +0200

    Issue #10, Create AnswerWrapper class

---
+++ app/src/main/java/com/rest/model/AnswerWrapper.java
@@ -0,0 +1,14 @@
+package com.rest.model;
+
+import java.util.List;
+
+/**
+ * Created by ehab on 2/14/15.
+ */
+public class AnswerWrapper {
+    List<Answer> answers;
+
+    public AnswerWrapper(List<Answer> answers) {
+        this.answers = answers;
+    }
+}
